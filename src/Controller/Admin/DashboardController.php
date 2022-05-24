39bfe4c3e145390afa4ff6fb7351417dcf63e693
Akif Güldemir <akifguldemir@gmail.com>
2022-05-24 19:06:15 +0300

    admin panelinde user işlemleri yapılabilir

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 use App\Entity\Blog;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -28,5 +29,9 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Blog', 'fas fa-list', Blog::class);
+        yield MenuItem::linkToCrud('User', 'fas fa-user', User::class);
+
+
+
     }
 }
