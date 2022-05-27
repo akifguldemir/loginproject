da3938ddc65015ecf49ead8e355dbc14f9381912
Akif Güldemir <akifguldemir@gmail.com>
2022-05-27 23:22:16 +0300

    blog crud

--- src/Entity/Blog.php
+++ src/Entity/Blog.php
@@ -48,6 +48,11 @@ class Blog
      */
     private $description;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $slug;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -109,6 +114,18 @@ class Blog
     public function setImageFile(?File $imageFile= null)
     {
         $this->imageFile= $imageFile;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(?string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
     }
 
 
