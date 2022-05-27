a2f3b16eaa9a2f3b9f60a868eddd62c910cf5cc4
Akif Güldemir <akifguldemir@gmail.com>
2022-05-28 00:22:12 +0300

    BlogRepository

---
+++ src/Repository/BlogRepository.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Blog;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\Exception\UnsupportedBlogException;
+use Symfony\Component\Security\Core\Blog\PasswordAuthenticatedBlogInterface;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
+
+
+/**
+ * @extends ServiceEntityRepository<Blog>
+ *
+ * @method Blog|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Blog|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Blog[]    findAll()
+ * @method Blog[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class BlogRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Blog::class);
+    }
+
+    public function add(Blog $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Blog $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    /**
+     * Used to upgrade (rehash) the Blog's password automatically over time.
+     */
+    public function upgradePassword(PasswordAuthenticatedBlogInterface $Blog, string $newHashedPassword): void
+    {
+        if (!$Blog instanceof Blog) {
+            throw new UnsupportedBlogException(sprintf('Instances of "%s" are not supported.', \get_class($Blog)));
+        }
+
+        $Blog->setPassword($newHashedPassword);
+
+        $this->add($Blog, true);
+    }
+
+//    /**
+//     * @return Blog[] Returns an array of Blog objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('u')
+//            ->andWhere('u.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('u.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Blog
+//    {
+//        return $this->createQueryBuilder('u')
+//            ->andWhere('u.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
